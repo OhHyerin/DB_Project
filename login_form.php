f6d29df016403e4f725ea626aed5c8aa670f80ee
OhHyerin <gpfls0506@gmail.com>
2020-06-12 16:39:06 +0900

    add user signup

--- login_form.php
+++ login_form.php
@@ -1,4 +1,27 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+<?php
+  include './dbconn.php';
+
+  $memberid=$_POST['user_id'];
+  $memberpassword=($_POST['user_password']);
+
+  $sql = "SELECT * FROM user_t WHERE id='$memberid' AND pw='$memberpassword'";
+  $sql = $sql. "values('$id', '$password', '$name', '$position')";
+  $res = $conn->query($sql);
+
+    $row = $res->fetch_array(MYSQLI_ASSOC);
+
+    if($row != null){
+      $_SESSION['ses_username'] = $row['user_name'];
+      echo $_SESSION['ses_username'].'님 안녕하세요<p/>';
+      echo '<a href="./loginpage.php">로그아웃하기</a>';
+    }
+
+    if($row == null){
+      echo("<script>location.href='loginerror.php';</script>");
+    }
+  ?>
 <html>
 <head>
   <title>로그인</title>
