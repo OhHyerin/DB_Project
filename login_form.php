1652df42cb4b3547b4ce15cf74869d53f76b5ebc
OhHyerin <gpfls0506@gmail.com>
2020-06-12 17:40:24 +0900

    before gather file

--- login_form.php
+++ login_form.php
@@ -1,30 +1,8 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 
-<?php
-  include './dbconn.php';
 
-  $memberid=$_POST['user_id'];
-  $memberpassword=($_POST['user_password']);
-
-  $sql = "SELECT * FROM user_t WHERE id='$memberid' AND pw='$memberpassword'";
-  $sql = $sql. "values('$id', '$password', '$name', '$position')";
-  $res = $conn->query($sql);
-
-    $row = $res->fetch_array(MYSQLI_ASSOC);
-
-    if($row != null){
-      $_SESSION['ses_username'] = $row['user_name'];
-      echo $_SESSION['ses_username'].'님 안녕하세요<p/>';
-      echo '<a href="./loginpage.php">로그아웃하기</a>';
-    }
-
-    if($row == null){
-      echo("<script>location.href='loginerror.php';</script>");
-    }
-  ?>
 <html>
 <head>
-  <title>로그인</title>
   <script>
     function checkform(){
       if(!document.login_form.user_id.value){
@@ -42,13 +20,36 @@
   </script>
 </head>
 <body>
-    <form action='./login2.php' name='login_form' method='post'>
+  <?php
+    include "./session.php";
+    include './dbconn.php';
+
+    $memberid=$_POST['user_id'];
+    $memberpassword=$_POST['user_password'];
+
+    $sql = "SELECT * FROM user_t WHERE id='$memberid' AND pw='$memberpassword'";
+    $sql = $sql. "values('$id', '$password', '$name', '$position')";
+    $res = $conn->query($sql);
+
+      $row = $res->fetch_array(MYSQLI_ASSOC);
+
+      if($row != null){
+        $_SESSION['ses_username'] = $row['user_name'];
+        echo $_SESSION['ses_username'].'님 안녕하세요<p/>';
+        echo '<a href="./loginpage.php">로그아웃하기</a>';
+      }
+
+      if($row == null){
+        echo("<script>location.href='./loginerror.php';</script>");
+      }
+    ?>
+    <!-- <form action='./login_form.php' name='login_form' method='post'>
       <CENTER>로그인</div>
         <label>아이디 : </label><input type="text" name="user_id" class="box"/><br>
         <label>비밀번호 : </label><input type="text" name="user_password" class="box"/></br>
 
-        <center><input type="button" value="로그인" OnClick="checkform();"/><br>
-        </form>
+        <center><input type="button" value="로그인" /><br>
+        </form> -->
 
 </body>
 </html>
--- loginpage.php
+++ loginpage.php
@@ -1,12 +1,13 @@
 <?
-include"session.php";
+include "session.php";
+include "dbconn.php";
 ?>
 
 <!doctype html>
 <html>
 <!-- print header -->
 <head>
-  <link rel="stylesheet" type="text/css" href="./login.css">
+  <link rel="stylesheet" type="text/css" href="./login.css?after">
   <link rel="stylesheet" type="text/css" href="./post.css">
 <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
 
@@ -24,7 +25,7 @@ $(document).ready( function() {
 <body>
 <div id="header"></div>
   <div id="showmorilogin">
-  <form action='./login2.php' name='login_form' method='post'>
+  <form action='./login_form.php' name='login_form' method='post'>
     <br>
     <br>
     <div class="login_rectangle">
@@ -32,9 +33,9 @@ $(document).ready( function() {
       <label>ID : </label><input type="text" name="user_id" class="box"/><br>
       <label>PW : </label><input type="password" name="user_password" class="box"/></br>
 <br><br>
-      <center><input type="submit" value="로그인" onClick="location='login_form.php'" class="login_button"/>
+      <center><input type="submit" value="로그인" class="login_button"/>
 <br><br>
-      <center><a href="./signuppage.html"><input type="button" value="회원가입"; class="submit_button"/></a></br>
+      <center><a href="./signuppage.html"><input type="button" value="회원가입" class="submit_button"/></a></br>
       </div>
   </div>
 
