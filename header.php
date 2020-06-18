5fdbe8f73177b61c272063c00750c5a58f64dde8
OhHyerin <gpfls0506@gmail.com>
2020-06-18 20:34:37 +0900

    merge file

--- header.php
+++ header.php
@@ -12,10 +12,12 @@
 	<div id="header">
 	<h1 class="title"><a href="./main_page.html">SHOW-MORI </a></h1>
 
-	<span id="search">
-	<button type="button" ><img src="./IMG/search.jpg" width="40" height="40" ></button>
-	<input type="text" id="search_text">
-	</span>
+	<form method="POST" action="search.php">
+      <span id="search">
+      <input type="text" id="search_text" name="srchTxt">
+      <button type="submit" ><img src="./IMG/search.jpg" width="40" height="40" ></button>
+      </span>
+   </form>
 
 	<button type="button" name="post" id="btn_post" onClick="location.href='writepostpage.html'">공연 게시하기</button>
 
@@ -33,20 +35,6 @@
 			<a href="./logout.php">로그아웃</a> | <a href="./myPage.html">마이페이지</a>
 		<?php }  ?>
 			</span>
-<!--			<button type="button" class="login_botton" onClick="location.href='logout.php'">로그아웃</button>/
-			<button type="button" class="login_botton" onClick="location.href='myPage.html'">마이페이지</button>
-			<button type="button" ><img src="./IMG/profile.jpg" width="40" height="40"  onclick="location.href='myPage.html'"></button>
-		</span>
-
-
-		<span class="login">
-		<button type="button" class="login_botton" onClick="location.href='loginpage.php'">로그인</button>/
-		<button type="button" class="login_botton" onClick="location.href='signuppage.html'">회원가입</button>
-		<button type="button" ><img src="./IMG/profile.jpg" width="40" height="40"  onclick="location.href='myPage.html'"></button>
-	</span>
--->
-
-
 
 	<img src="./IMG/headerLine.png" id="line">
 	</div>
--- printImg.php
+++ printImg.php
@@ -1,20 +1,24 @@
 <?
 include './dbconn.php';
 
+
+
 	$qInfo = "SELECT S_TITLE,S_POSTER,S_GOALSUM,S_DEADLINE,S_PRM from post_t;";
 	$rPoster = mysqli_query($conn,$qInfo);
 	$nDate = date('Y-m-d');
-
+	$cnt=1;
 
 	while($row=mysqli_fetch_array($rPoster)){
-		$title = $row['S_TITLE'];//공연제목
-		$show_p = $row['S_PRM']; //POST_T.S_PRM
-		$poster = $row['S_POSTER'];
+		$title = $row['S_TITLE'];
+		$show_p = $row['S_PRM'];
 
-		//GET SUM(D_MONEY) BY BACKING
+		//GET SUM(D_MONEY) BY DONATION
 		$qSumMoney = "SELECT S_PRM, sum(d_money) as sum from d_info_t WHERE S_PRM='".$show_p."';";
 		$rSumMoney = mysqli_query($conn,$qSumMoney);
+
+
 		$row2 = mysqli_fetch_array($rSumMoney);
+		
 
 		$leftSum = ($row['S_GOALSUM']-$row2['sum']);//goalsum - donated sum
 		$leftDate = intval((strtotime($row['S_DEADLINE'])-strtotime($nDate)) / 86400);
@@ -22,23 +26,74 @@ include './dbconn.php';
 
 		$percentage =round($row2['sum']/$row['S_GOALSUM'],2);
 
+		if($leftDate<=0){
+			$qDet = "DELETE FROM D_INFO_T WHERE S_PRM='".$show_p."';";
+			$qDet .= "DELETE FROM s_date_t WHERE S_PRM='".$show_p."';";
+			$qDet .= "DELETE FROM post_t WHERE S_PRM='".$show_p."';";
+			$rDet = mysqli_multi_query($conn,$qDet);
+		}
 
-		echo "
-			<td>
-			<input type='image' src='./data/IMG/$poster' onClick='./s_info_page.php'  width='200'>
 
-    	//<a href='./s_info_page.php'><img src='./data/IMG/$poster'/></a>
-			<a href='./s_info_page.php'><p id='s_title'>$title</p></a>
-			<p class='t_content'>$leftSum 남음
-				&nbsp;&nbsp;
-				$percentage %</a>
-			</p>
-			<p class='t_content'>마감
-				&nbsp;&nbsp;&nbsp;&nbsp;
-			 	<a>D-$leftDate day</a>
-			 </p>
-			</td>";
+		if($cnt%4==1){
+			echo"
+				<tr>
+				<form method='POST' action='./s_info_page.php'>
+					<td>
+						<a href='./s_info_page.php'><img src='./data/IMG/".$row['S_POSTER']."'/></a>
+						<a href='./s_info_page.php?title=<?$title?>'><p id='s_title' name='title'>$title</p></a>
 
+						<p class='t_content'>$leftSum 남음
+							&nbsp;&nbsp;
+							$percentage %</a>
+						</p>
+						<p class='t_content'>마감
+							&nbsp;&nbsp;&nbsp;&nbsp;
+						 	<a name='print_leftDate'>D-$leftDate day</a>
+						 </p>
+					</td>
+				</form>
+				";
+				$cnt++;
+		}
+		 else if($cnt%4==0){
+			echo"
+				<form method='POST' action='./s_info_page.php'>
+					<td>
+						<a href='./s_info_page.php'><img src='./data/IMG/".$row['S_POSTER']."'/></a>
+						<a href='./s_info_page.php'><p id='s_title' name='print_title'>$title</p></a>
+						<p class='t_content'>$leftSum 남음
+							&nbsp;&nbsp;
+							$percentage %</a>
+						</p>
+						<p class='t_content'>마감
+							&nbsp;&nbsp;&nbsp;&nbsp;
+						 	<a name='print_leftDate'>D-$leftDate day</a>
+						 </p>
+					 </td>
+				 </form>
+				</tr>";
+				$cnt++;
+		}
+		else{
+			echo"
+				<form method='POST' action='s_info_page.php'>
+					<td>
+						<a href='./s_info_page.php'><img src='./data/IMG/".$row['S_POSTER']."'/></a>
+						<a href='./s_info_page.php'><p id='s_title' name='print_title'>$title</p></a>
+						<p class='t_content'>$leftSum 남음
+							&nbsp;&nbsp;
+							$percentage %</a>
+						</p>
+						<p class='t_content'>마감
+							&nbsp;&nbsp;&nbsp;&nbsp;
+						 	<a name='print_leftDate'>D-$leftDate day</a>
+						 </p>
+					 </td>
+				 </form>
+
+				";
+				$cnt++;
+			}
 	}//print info of show -tuples from DB
 mysqli_close($conn);
 ?>
